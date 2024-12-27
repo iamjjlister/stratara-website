6d22f9d918ca7b6a219ce681472dab19991f77d3
iamjjlister <iamjjlister@gmail.com>
2024-12-27 21:39:26 +0000

    Initial website setup with Header and Hero components

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/layout/Header';
+import Hero from './components/sections/Hero';
+
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
